251023f85ac22ac0f3c8788cf8ab383b3455c589
faizkhan731 <faizkhan78776@gmail.com>
2026-03-22 14:43:07 +0530

    Revert "remove f aPi"
    This reverts commit 2f35a0f573a703659f868ed621590e650c402249, reversing
    changes made to 9edeeed3a8be31b0264afedaeccf4fc5c860df48.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,8 +5,6 @@ const BASE_URL =
   import.meta.env.VITE_API_URL ||
   // "https://flipkart-clone-6-rj5o.onrender.com/api";
   "https://flipkart-clone-9.onrender.com/api";
-
-
 // Session management
 const getSessionId = () => {
   let sid = localStorage.getItem('fk_session_id');
@@ -32,7 +30,7 @@ api.interceptors.request.use((config) => {
 export const productService = {
   getAll: (params = {}) => api.get('/products', { params }),
   getById: (id) => api.get(`/products/${id}`),
-  // getFeatured: () => api.get('/products/featured'),
+  getFeatured: () => api.get('/products/featured'),
 };
 
 // ─── Categories ──────────────────────────────────────────────────────────────
